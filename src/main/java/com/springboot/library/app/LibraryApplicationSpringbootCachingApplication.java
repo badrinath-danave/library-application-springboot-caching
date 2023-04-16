ff24c754b33f3513dc64cd9e82e339e3b4439aec
Badrinath Danave <badrinathdanave@gmail.com>
2023-04-16 13:16:43 +0530

    initial project setup with dependencies

---
+++ src/main/java/com/springboot/library/app/LibraryApplicationSpringbootCachingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.library.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplicationSpringbootCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplicationSpringbootCachingApplication.class, args);
+	}
+
+}
